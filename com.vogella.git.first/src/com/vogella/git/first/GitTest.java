06920e5d242e6130d1964bb3f67c112a86f0aebc
Patrick Bonnet <patrick.bonnet.bry@club.fr>
2016-06-29 23:20:11 +0200

    still change the message
    This change is just for creating a new commit after the previous merge
    commit

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -3,7 +3,7 @@ package com.vogella.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Git is nice and cool");
+		System.out.println("Git is very nice and cool !");
 	}
 
 }
